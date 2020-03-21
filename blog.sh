7f7007e7360b9f494f99e8de0fe71ee30c0c278c
Cooomma <payton@cooomma.info>
2020-03-22 00:46:24 +0800

    update run script

--- blog.sh
+++ blog.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo $GCLOUD_SERVICE_KEY | base64 --decode >/app/key.json
+export GOOGLE_APPLICATION_CREDENTIALS=/app/key.json
 python3 blog.py check-blog-update
 python3 blog.py crawl_blogs
